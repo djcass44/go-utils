4a7b1defdbbe8fde8b54dbaa03934b2c8fb187bf
Paweł Słomka <ppawelslomka@gmail.com>
2020-10-06 03:10:36 +0200

    Add missing status code group checks (#3)
    Fixes https://github.com/djcass44/go-utils/issues/2

--- pkg/httputils/codes.go
+++ pkg/httputils/codes.go
@@ -17,14 +17,47 @@
 
 package httputils
 
-import "net/http"
+const (
+	statusCodeGroupInfo      = 100
+	statusCodeGroupSuccess   = 200
+	statusCodeGroupRedirect  = 300
+	statusCodeGroupClientErr = 400
+	statusCodeGroupServerErr = 500
+)
 
-// IsHttpError returns true if a status code is 400 or above
+var (
+	// IsHttpInformation returns true if a status code is of the 1xx range (100 - 199).
+	IsHttpInformation = statusGroupEqual(statusCodeGroupInfo)
+
+	// IsHttpSuccess returns true if a status code is of the 2xx range (200 - 299).
+	IsHttpSuccess = statusGroupEqual(statusCodeGroupSuccess)
+
+	// IsHttpRedirect returns true if a status code is of the 3xx range (300 - 399).
+	IsHttpRedirect = statusGroupEqual(statusCodeGroupRedirect)
+
+	// IsHttpClientError returns true if a status code is of the 4xx range (400 - 499).
+	IsHttpClientError = statusGroupEqual(statusCodeGroupClientErr)
+
+	// IsHttpServerError returns true if a status code is of the 5xx range (500 - 599).
+	IsHttpServerError = statusGroupEqual(statusCodeGroupServerErr)
+)
+
+// IsHttpError returns true if a status code is 4xx or 5xx.
 func IsHttpError(code int) bool {
-	return code > 399
+	return IsHttpClientError(code) || IsHttpServerError(code)
 }
 
-// IsHttpSuccess returns true if a status code is of the 2xx range (200 - 299)
-func IsHttpSuccess(code int) bool {
-	return code >= http.StatusOK && code <= 299
+// Returns a function that checks if a given status code belongs to a status code group.
+func statusGroupEqual(groupCode int) func(int) bool {
+	// statusCodeGroup returns a status code group (1xx, 2xx, ...) for a given status code.
+	statusCodeGroup := func(code int) int {
+		// Status code group is indicated by the first digit, ie. a result of integer division is enough.
+		firstDigit := code / 100
+
+		return firstDigit * 100
+	}
+
+	return func(code int) bool {
+		return statusCodeGroup(code) == groupCode
+	}
 }
